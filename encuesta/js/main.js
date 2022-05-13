69a7f80751ac11dee60c04e9919ec3fffbc0c01e
Virginia Garcia <85966206+virginia-garcia@users.noreply.github.com>
2022-05-13 05:33:13 -0300

    Add files via upload

---
+++ encuesta/js/main.js
@@ -0,0 +1,38 @@
+window.onload = function () {
+    var s = document.getElementById("pingStar"),
+     m = document.getElementById('dir'),
+     n = s.getElementsByTagName("li"),
+                        input = document.getElementById ('startP'); // Guarda el valor seleccionado
+    clearAll = function () {
+     for (var i = 0; i < n.length; i++) {
+      n[i].className = '';
+     }
+    }
+    for (var i = 0; i < n.length; i++) {
+     n[i].onclick = function () {
+      var q = this.getAttribute("rel");
+      clearAll();
+      input.value = q;
+      for (var i = 0; i < q; i++) {
+       n[i].className = 'on';
+      }
+      m.innerHTML = this.getAttribute("title");
+     }
+     n[i].onmouseover = function () {
+      var q = this.getAttribute("rel");
+      clearAll();
+      for (var i = 0; i < q; i++) {
+       n[i].className = 'on';
+      }
+     }
+     n[i].onmouseout = function () {
+      clearAll();
+      for (var i = 0; i < input.value; i++) {
+       n[i].className = 'on';
+      }
+     }
+    }
+   }
+
+
+/*Boton enviar*/
